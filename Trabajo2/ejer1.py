554b7b5f0270f01b50fec1266a8f7d05c09bd524
edinsongelvez <49883896+edinsongelvez@users.noreply.github.com>
2019-05-04 10:57:54 -0500

    Trabajo2
    Hago envio del trabajo 2

---
+++ Trabajo2/ejer1.py
@@ -0,0 +1,9 @@
+import math as mt
+
+r=float(input("digite el valor de r"))
+angulo=float(input("digite el valor del ángulo"))
+x=r*mt.cos(angulo/180*mt.pi)
+y=r*mt.sin(angulo/180*mt.pi)
+print("el resultado de x es ",x)
+print("el resultado de y es ",y)
+
---
+++ Trabajo2/ejerc1orbitaplanetaria.py
@@ -0,0 +1,28 @@
+G=6.67e-11
+M=1.989e30
+pi=3.1416
+
+rp=float(input("digite el radio del perihelio:"))
+vp=float(input("digite la velocidad del perihelio:"))
+
+va=(((2*G*M)/(vp*rp))+(((2*G*M)/(vp*rp))**2+4*((vp**2-2*G*M)/rp))**1/2)/2
+ra=(rp*vp)/va
+a=(rp+ra)/2
+b=(rp*ra)**1/2
+T=(2*pi*a*b)/(rp*vp)
+e=(ra-rp)/(ra+rp)
+
+print("la velocidad del afelio es:" ,va)
+print("el radio del afelio:" ,ra)
+print("el semieje mayor es:" ,a)
+print("el semieje menor es:" ,b)
+print("el periodo orbital es:" ,T)
+print("la excentricidad orbital es:" ,e)
+
+
+
+
+
+
+
+
